bc5ce746cee7c1928f7a71eb190aff5957c43d22
baghdat-bm <baghdat.bm@gmail.com>
2024-12-09 18:17:16 +0500

    дошел до раздела 04, урок 07 (+-)

--- vuejs-masterclass-2024/vite.config.ts
+++ vuejs-masterclass-2024/vite.config.ts
@@ -4,12 +4,25 @@ import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     VueRouter(),
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (element) => element.startsWith('iconify-icon')
+        }
+      }
+    }),
     vueDevTools(),
   ],
   resolve: {
